81a1eecf77d714eb989d100cd53bb2c135931ff3
polopalay <60103759+polopalay@users.noreply.github.com>
2025-05-05 15:43:12 +0700

    update

--- fine_tuning_using_lora.py
+++ fine_tuning_using_lora.py
@@ -114,6 +114,6 @@ qa_pipeline = pipeline("text-generation", model="deepseek_finetuned_model")
 
 question = "Tên gọi nào được Phạm Văn Đồng sử dụng khi làm Phó chủ nhiệm cơ quan Biện sự xứ tại Quế Lâm?"
 context = "Lâm Bá Kiệt"
-
+result = qa_pipeline(question + " " + context, return_full_text=False)
 result = print(result[0]["generated_text"])
 print(result)
